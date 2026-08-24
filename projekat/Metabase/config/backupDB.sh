7918d27ad31577d9a75998ee24b55cd92f44ef37
Davor Homa <davorhoma@gmail.com>
2026-08-24 13:23:47 +0200

    Delete projekat/Metabase directory

--- projekat/Metabase/config/backupDB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "#### Creating backup of the Metabase database..."
-pg_dump -h 127.0.0.1 -d metabasedb -U $POSTGRES_USER -w -f /config/backup/metabasedb-yt-tt-analysis.dump -F c
-if [ $? -eq 0 ]; then
-    echo "#### Backup creation completed successfully!"
-else
-    echo "#### Backup creation failed!"
-fi
